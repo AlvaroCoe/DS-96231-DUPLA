67ffceacece68573ef9ba0633b72b89a7e9370ca
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-09 21:18:44 -0300

    Criei o JSX do Header.

--- projeto-git-dupla/src/componets/Header/index.jsx
+++ projeto-git-dupla/src/componets/Header/index.jsx
@@ -1,1 +1,36 @@
+import { Link, NavLink } from 'react-router-dom'
 import './style.css'
+
+export default function Header() {
+    return (
+        <header className="header-container">
+            
+            <div className="header-logo-box">
+                <Link to='/' className='header-logo'>
+                    <h1>🐾 Pet & Shop</h1>
+                </Link>
+            </div>
+
+            <div className="header-busca">
+                <input type="text" placeholder="O que seu pet precisa hoje?" />
+            </div>
+
+            <nav className="header-nav">
+                <NavLink to='/'>Home</NavLink>
+                <NavLink to='/produtos'>Produtos</NavLink>
+                <NavLink to='/servicos'>Serviços</NavLink>
+                <NavLink to='/sobre-nos'>Sobre Nós</NavLink>
+            </nav>
+
+            <div className="header-acoes">
+                <Link to='/carrinho' className="btn-carrinho">
+                    🛒 <span>0</span>
+                </Link>
+                <Link to='/login' className="btn-entrar">
+                    Entrar
+                </Link>
+            </div>
+
+        </header>
+    )
+}
