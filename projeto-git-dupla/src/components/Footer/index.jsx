2eb8a599f942e69b9697c72b9c53220373b63221
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-06-10 21:22:49 -0300

    atualização Footer

--- projeto-git-dupla/src/components/Footer/index.jsx
+++ projeto-git-dupla/src/components/Footer/index.jsx
@@ -7,15 +7,6 @@ export default function Footer() {
       <div className="footer-container">
 
         <div className="footer-col">
-          <h2>🐾 Pet & Shop</h2>
-          <p>
-            Pet Shop e Clínica Veterinária dedicada ao bem-estar animal,
-            oferecendo produtos de qualidade e atendimento especializado para
-            seu melhor amigo.
-          </p>
-        </div>
-
-        <div className="footer-col">
           <h3>Contato</h3>
           <p>📍 Rua dos Animais, 123</p>
           <p>📍 Salvador - BA</p>
@@ -29,16 +20,6 @@ export default function Footer() {
           <p>Sábado: 08h às 14h</p>
           <p>Domingo: Fechado</p>
           <p>Emergências: 24 horas</p>
-        </div>
-
-        <div className="footer-col footer-navigation-block">
-          <h3>Links Rápidos</h3>
-          <nav className="footer-nav-links">
-            <NavLink to='/'>Home</NavLink>
-            <NavLink to='/produtos'>Produtos</NavLink>
-            <NavLink to='/servicos'>Serviços</NavLink>
-            <NavLink to='/sobrenos'>Sobre nós</NavLink>
-          </nav>
         </div>
 
       </div>
